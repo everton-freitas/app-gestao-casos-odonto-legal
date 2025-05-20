417e98264212d9b5b73c7e5320b59ea101a1355d
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-20 10:26:04 -0300

    refatora estrutura do App e adiciona rotas de navegação

--- MeuApp/src/routes.js
+++ MeuApp/src/routes.js
@@ -8,12 +8,12 @@ import Details from './pages/Details';
 const Stack = createStackNavigator();
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Details" component={Details} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Details" component={Details} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
