4408bdd9aa910de260967a50a544968150b9aa04
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-20 11:43:09 -0300

    adiciona carregamento de fontes Roboto com ActivityIndicator no componente App

--- MeuApp/src/App.js
+++ MeuApp/src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Routes from './routes';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+
+const [fontsLoaded] = useFonts({
+  Roboto_400Regular,
+  Roboto_700Bold,
+});
+
+if (!fontsLoaded) {
+  return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+}
 
 export default function App() {
 	return (
