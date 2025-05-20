746dfcb15504dbeae2213057c972dceb5393cb01
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-20 10:43:45 -0300

    refatora rotas do aplicativo para incluir tela de login como rota inicial

--- MeuApp/src/routes.js
+++ MeuApp/src/routes.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import Home from './pages/Home';
-import Details from './pages/Details';
+import Login from './pages/login';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Home" component={Home} />
-				<Stack.Screen name="Details" component={Details} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
