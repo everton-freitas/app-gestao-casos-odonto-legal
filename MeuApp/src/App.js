92cff5ed8bf653150992a04c1c6784d6c9a0c0a3
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-20 11:46:33 -0300

    refatora o componente App para melhorar a legibilidade do carregamento de fontes

--- MeuApp/src/App.js
+++ MeuApp/src/App.js
@@ -4,20 +4,20 @@ import Routes from './routes';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ActivityIndicator } from 'react-native';
 
-const [fontsLoaded] = useFonts({
-  Roboto_400Regular,
-  Roboto_700Bold,
-});
-
-if (!fontsLoaded) {
-  return <ActivityIndicator size="large" style={{ flex: 1 }} />;
-}
-
 export default function App() {
-	return (
-		<>
-			<StatusBar style="auto" />
-			<Routes />
-		</>
-	);
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Routes />
+    </>
+  );
 }
