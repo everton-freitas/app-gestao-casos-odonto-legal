2a8d80dd6a77c8db8be01fe167be3023f68fde2b
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-29 15:30:30 -0300

    refatora a navegação para incluir opções de tela e melhorar a legibilidade do código

--- MeuApp/src/routes.js
+++ MeuApp/src/routes.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Image } from 'react-native';
 import Login from './pages/login';
-import NewPassword from './pages/new_password'; 
+import NewPassword from './pages/new_password';
 import { headerStyles } from './pages/login/styles';
 import { COLORS } from './colors';
 
@@ -12,24 +12,23 @@ const Stack = createStackNavigator();
 export default function Routes() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Login">
-				<Stack.Screen
-					name="Login"
-					component={Login}
-					options={{
-						title: 'Dentalysis',
-						headerStyle: headerStyles.header,
-						headerTintColor: COLORS.primary,
-						headerTitleAlign: 'left',
-						headerLeft: () => (
-							<Image
-								source={require('./assets/logo.png')}
-								style={headerStyles.logo}
-								resizeMode="contain"
-							/>
-						),
-					}}
-				/>
+			<Stack.Navigator
+				initialRouteName="Login"
+				screenOptions={{
+					title: 'Dentalysis',
+					headerStyle: headerStyles.header,
+					headerTintColor: COLORS.primary,
+					headerTitleAlign: 'left',
+					headerLeft: () => (
+						<Image
+							source={require('./assets/logo.png')}
+							style={headerStyles.logo}
+							resizeMode="contain"
+						/>
+					),
+				}}
+			>
+				<Stack.Screen name="Login" component={Login} />
 				<Stack.Screen name="NewPassword" component={NewPassword} />
 			</Stack.Navigator>
 		</NavigationContainer>
