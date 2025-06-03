917d71296d6efb927c8dcd6f7cef1a7dea70e2f6
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-03 16:26:52 -0300

    refactor: improve code formatting and structure in App component

--- MeuApp/src/App.js
+++ MeuApp/src/App.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Routes from './Routes';
 import {
-	useFonts,
-	Roboto_400Regular,
-	Roboto_700Bold,
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 import { ActivityIndicator } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Roboto_400Regular,
-		Roboto_700Bold,
-	});
+    const [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_700Bold,
+    });
 
-	if (!fontsLoaded) {
-		return <ActivityIndicator size="large" style={{ flex: 1 }} />;
-	}
+    if (!fontsLoaded) {
+        return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+    }
 
-	return (
-		<>
-			<StatusBar style="auto" />
-			<Routes />
-		</>
-	);
+    return (
+        <PaperProvider>
+            <StatusBar style="auto" />
+            <Routes />
+        </PaperProvider>
+    );
 }
