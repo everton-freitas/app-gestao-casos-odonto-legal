c7eb8ae23d43dbda16e390e583b970ad3f329758
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-10 22:01:06 -0300

    feat: refactor Home component to remove header and move styles to a separate file

--- MeuApp/src/pages/home/index.js
+++ MeuApp/src/pages/home/index.js
@@ -3,32 +3,16 @@ import { ScrollView, View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import StatusPieChart from '../../components/charts/StatusPieCharts';
 import CasesBarChart from '../../components/charts/CasesBarChart';
 import VictimsStatusChart from '../../components/charts/VictmsStatusChart';
+import styles from './styles';
 
 export default function Home() {
 	return (
 		<SafeAreaView style={styles.safeArea}>
 			<ScrollView contentContainerStyle={styles.container}>
-				<Text style={styles.header}>Dashboard</Text>
 				<StatusPieChart />
 				<CasesBarChart />
 				<VictimsStatusChart />
 			</ScrollView>
 		</SafeAreaView>
 	);
-}
-
-const styles = StyleSheet.create({
-	safeArea: {
-		flex: 1,
-		backgroundColor: '#f0f4f7',
-	},
-	container: {
-		padding: 20,
-	},
-	header: {
-		fontSize: 28,
-		fontWeight: 'bold',
-		color: '#012130',
-		marginBottom: 20,
-	},
-});
+};
---
+++ MeuApp/src/pages/home/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import {COLORS} from '../../Colors';
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: COLORS.evidence,
+    },
+    container: {
+        padding: 20,
+    },
+});
+
+export default styles;
